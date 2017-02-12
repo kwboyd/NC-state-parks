3f43541bddd13916374298f1c900c9cf5d42064d
Kate Boyd <kwboyd95@gmail.com>
2017-02-12 12:01:21 -0500

    Loaded JSON data.

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,25 @@
 </template>
 
 <script>
+import axios from 'axios'
 import mapgoogle from './components/mapgoogle'
 export default {
   name: 'app',
   components: {
     mapgoogle
+  },
+  data () {
+    return {
+      parks: []
+    }
+  },
+  mounted() {
+    axios.get('/static/parks.json')
+      .then((response) => {
+        console.log(response.data)
+        this.parks = response.data
+        console.log(this.parks[0].name)
+    })
   }
 }
 </script>
