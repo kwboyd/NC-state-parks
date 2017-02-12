a7a3201ff30c22d0b0ec68b5403d41b71be37b7d
Kate Boyd <kwboyd95@gmail.com>
2017-02-11 20:20:31 -0500

    Map can load.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <hello></hello>
+    <mapgoogle></mapgoogle>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
+var googlescript = document.createElement('script')
+googlescript.setAttribute('type', 'text/javascript')
+googlescript.async = true
+googlescript.defer = true
+googlescript.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAvSWIBEUD5mSVcV5U1Ev51tkMp0ugWJk8')
+document.getElementsByTagName('head')[0].appendChild(googlescript)
+import mapgoogle from './components/mapgoogle'
 export default {
   name: 'app',
   components: {
-    Hello
+    mapgoogle
   }
 }
 </script>
